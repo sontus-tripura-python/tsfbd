539e7c6df76597c6134ba936ede4bdfc55c1ac38
sontus-tripura-python <sontustrp.com@gmail.com>
2021-03-31 20:09:15 +0600

    css fixed

--- Photo_app/models.py
+++ Photo_app/models.py
@@ -10,7 +10,7 @@ class Category(models.Model):
 class Photo(models.Model):
     category = models.ForeignKey(
         Category, on_delete=models.SET_NULL, null=True, blank=True)
-    image = models.ImageField(null=False, blank=False)
+    image = models.ImageField(upload_to='Gallery', null=False, blank=False)
     description = models.TextField()
 
     def __str__(self):
